dc95fd04f566d4eba933be5a98a4f35063c50439
BryanPanchiZapata <bryanlife2530@gmail.com>
2024-08-31 19:36:14 -0500

    Se recupero los valores de las cajas de texto y se creo el objeto cuenta

--- cuentas.js
+++ cuentas.js
@@ -1,43 +1,63 @@
-cuentas=[
-    {numeroCuenta:"02234567", cedula:"1714616123",nombre:"Juan",apellido:"Perez",saldo:0.0},
-    {numeroCuenta:"02345211",cedula:"1281238233",nombre:"Felipe",apellido:"Caicedo",saldo:0.0}
-]
+cuentas = [
+  {
+    numeroCuenta: "02234567",
+    cedula: "1714616123",
+    nombre: "Juan",
+    apellido: "Perez",
+    saldo: 0.0,
+  },
+  {
+    numeroCuenta: "02345211",
+    cedula: "1281238233",
+    nombre: "Felipe",
+    apellido: "Caicedo",
+    saldo: 0.0,
+  },
+];
 
-cargar=function(){
-    mostrarComponente("divCuentas");
-    ocultarComponente("divMovimientos");
-    ocultarComponente("divTransacciones");
-    
-}
+cargar = function () {
+  mostrarComponente("divCuentas");
+  ocultarComponente("divMovimientos");
+  ocultarComponente("divTransacciones");
+};
 
-mostrarCuentas=function(){
-    /*
-        Muestra en pantalla una tabla con la información de todas las cuentas del arreglo.
-        Columnas: NUMERO CUENTA, NOMBRE, SALDO
-        En la columna NOMBRE concatenar el nombre y el apellido
-    */
-}
+mostrarCuentas = function () {
+  /*
+      Muestra en pantalla una tabla con la información de todas las cuentas del arreglo.
+      Columnas: NUMERO CUENTA, NOMBRE, SALDO
+      En la columna NOMBRE concatenar el nombre y el apellido
+  */
+};
 
 /*
-    Busca la cuenta en el arreglo en función del número de cuenta,
-    si existe retorna el objeto cuenta, caso contrario retorna null. 
+  Busca la cuenta en el arreglo en función del número de cuenta,
+  si existe retorna el objeto cuenta, caso contrario retorna null. 
 */
-buscarCuenta=function(numeroCuenta){
-
-}
+buscarCuenta = function (numeroCuenta) {};
 
 /*
     Agrega una cuenta al arreglo, solamente si no existe otra cuenta con el mismo numero.
     No retorna nada
 */
-agregarCuenta=function(cuenta){
-    //Si ya existe mostrar un alert CUENTA EXISTENTE
-    //Si se agrega, mostrar un alert CUENTA AGREGADA
-}
+mostrarCuentas = function () {
+  //Si ya existe mostrar un alert CUENTA EXISTENTE
+  //Si se agrega, mostrar un alert CUENTA AGREGADA
+};
 
-agregar=function(){
-    //Toma los valores de las cajas de texto, sin validaciones
-    //Crea un objeto cuenta y agrega los atributos con los valores de las cajas respectivas
-    //Invoca a agregarCuenta
-    //Invoca a mostrarCuentas
-}
+agregar = function () {
+  //Toma los valores de las cajas de texto, sin validaciones
+  let valorCedula = recuperarTexto("lblCedula");
+  let valorNombre = recuperarTexto("lblNombre");
+  let valorApellido = recuperarTexto("lblApellido");
+  let valorCuenta = recuperarTexto("lblCuenta");
+  //Crea un objeto cuenta y agrega los atributos con los valores de las cajas respectivas
+  let cuenta = {
+    numeroCuenta: valorCuenta,
+    cedula: valorCedula,
+    nombre: valorNombre,
+    apellido: valorApellido,
+    saldo: 0,
+  };
+  //Invoca a agregarCuenta
+  //Invoca a mostrarCuentas
+};
